6e69b22a3e6d0f6890707b16de6bdc92264eac77
Funya1337 <funya7654@gmail.com>
2022-04-01 21:29:46 +0300

    feat(client, server): drawing options, socket server

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from tkinter import *
 import random
 import time
+import socket
+import select
+import errno
 
 class GUI:
     default_color = '#AAAAFF'
@@ -27,9 +30,10 @@ class GUI:
         self.createButton(300, 20, "Selection sort", self.selectionSort)
         self.createLabel(0, 55)
         self.createSpeedScale(100, 38)
-        self.createButton(215, 50, "Back", self.test)
-        self.createButton(260, 50, "Forward", self.test)
+        self.createButton(215, 50, "Quick sort", self.startQuickSort)
+        self.createButton(300, 50, "Insertion sort", self.insertionSort)
         self.createDrawingBtn()
+        self.createEraseBtn()
 
     def getValue(self, event):
         self.scaleValue = self.scale.get()
@@ -43,7 +47,40 @@ class GUI:
         #self.canvas.itemconfig(items[0], fill = 'green')
 
     def useDrawingMode(self):
-        print(self.root)
+        self.old_x = None
+        self.old_y = None
+        self.line_width = 5
+        self.color = "blue"
+        self.eraser_on = False
+        self.active_button = self.drawingBtn
+        self.canvas.bind('<B1-Motion>', self.paint)
+        self.canvas.bind('<ButtonRelease-1>', self.reset)
+
+    def activate_button(self, some_button, eraser_mode=False):
+        self.active_button = some_button
+        self.eraser_on = eraser_mode
+    
+    def useErase(self):
+        self.activate_button(self.eraser_button, eraser_mode=True)
+
+    def use_pen(self):
+        self.activate_button(self.pen_button)
+    
+    def paint(self, event):
+        if self.eraser_on:
+            paint_color = 'SystemButtonFace'
+            self.line_width = 15
+        else:
+            paint_color = self.color
+        if self.old_x and self.old_y:
+            self.canvas.create_line(self.old_x, self.old_y, event.x, event.y,
+                               width=self.line_width, fill=paint_color,
+                               capstyle=ROUND, smooth=TRUE, splinesteps=36)
+        self.old_x = event.x
+        self.old_y = event.y
+    
+    def reset(self, event):
+        self.old_x, self.old_y = None, None
 
     def getSpeed(self, event):
         if event == '1':
@@ -58,8 +95,12 @@ class GUI:
         self.speedScale.place(x=cord_x, y=cord_y)
 
     def createDrawingBtn(self):
-        btn = Button(self.root, text = 'Drawing Mode', command = self.useDrawingMode, width = 12, height = 3)
-        btn.place(x = 390, y = 20)
+        self.drawingBtn = Button(self.root, text = 'Drawing Mode', command = self.useDrawingMode, width = 12, height = 3)
+        self.drawingBtn.place(x = 390, y = 20)
+    
+    def createEraseBtn(self):
+        self.eraser_button = Button(self.root, text = 'Erase Mode', command = self.useErase, width = 12, height = 3)
+        self.eraser_button.place(x = 500, y = 20)
 
     def createLabel(self, cord_x, cord_y):
         label = Label(self.canvas, text='Animation Speed:')
@@ -80,7 +121,7 @@ class GUI:
         print(self.scaleValue)
         self.canvas.delete('all')
         default_x = 50
-        default_y = 300
+        default_y = 350
         default_width = 15
         self.array = []
         cnt = 0
@@ -89,6 +130,7 @@ class GUI:
             self.createRectangle(default_x + cnt, default_y, default_width, element * 2)
             self.array.append(element)
             cnt += 20
+        self.items = list(self.canvas.find_all())
 
     def reDrawChart(self, idx1, idx2):
         mult = 20
@@ -110,6 +152,7 @@ class GUI:
         time.sleep(self.speed - 0.01)
 
     def finalHightLight(self):
+        print(self.items)
         for i in self.items:
             self.canvas.itemconfig(i, fill = GUI.selection_color)
             self.canvas.update()
@@ -119,7 +162,6 @@ class GUI:
             self.canvas.itemconfig(i, fill= GUI.default_color)
 
     def bubbleSort(self):
-        self.items = list(self.canvas.find_all())
         for i in range(0, len(self.array) - 1):
             for j in range(len(self.array) - 1):
                 if self.array[j] > self.array[j + 1]:
@@ -128,7 +170,6 @@ class GUI:
         self.finalHightLight()
 
     def selectionSort(self):
-        self.items = list(self.canvas.find_all())
         for i in range(len(self.array)):
             min_idx = i
             for j in range(i + 1, len(self.array)):
@@ -136,6 +177,40 @@ class GUI:
                     min_idx = j
             self.array[i], self.array[min_idx] = self.array[min_idx], self.array[i]
             self.reDrawChart(i, min_idx)
+        self.finalHightLight()
+
+    def partition(self, array, low, high):
+      pivot = array[high]
+      i = low - 1
+      for j in range(low, high):
+        if array[j] <= pivot:
+          i = i + 1
+          (array[i], array[j]) = (array[j], array[i])
+          self.reDrawChart(i, j)
+
+      (array[i + 1], array[high]) = (array[high], array[i + 1])
+      self.reDrawChart(i + 1, high)
+      return i + 1
+
+    def quickSort(self, array, low, high):
+      if low < high:
+        pi = self.partition(array, low, high)
+        self.quickSort(array, low, pi - 1)
+        self.quickSort(array, pi + 1, high)
+
+    def startQuickSort(self):
+        self.quickSort(self.array, 0, len(self.array) - 1)
+        self.finalHightLight()
+
+    def insertionSort(self):
+        for i in range(1, len(self.array)):
+            key = self.array[i]
+            j = i-1
+            while j >= 0 and key < self.array[j] :
+                    self.array[j + 1] = self.array[j]
+                    self.reDrawChart(j + 1, j)
+                    j -= 1
+            self.array[j + 1] = key
         self.finalHightLight()
 
 gui = GUI()
